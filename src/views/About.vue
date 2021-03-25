2247dd6c6af9d6c751993a8bc4440c055f4c4ed6
xiao <wswplay2006@126.com>
2021-03-25 15:29:17 +0800

    init pro

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    这里有文字的啊，是不是
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="less" scoped></style>
