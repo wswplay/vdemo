016e95c65fc00c808aa816e6e7780b0a98e8505e
xiao <183627391@qq.com>
2021-10-02 21:43:59 +0800

    sth opt

--- src/xvui/xcollapse-tree.vue
+++ src/xvui/xcollapse-tree.vue
@@ -9,44 +9,11 @@
       text-color="#000"
     >
       <Xelement v-for="item in list" :key="item.path" :info="item" />
-      <!-- <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>技术栈</span>
-        </template>
-        <el-submenu index="1-1">
-          <template slot="title">Vue生态</template>
-          <el-submenu index="1-1-1">
-            <template slot="title">Vue</template>
-            <el-menu-item index="1-1-1-1">2.0</el-menu-item>
-            <el-menu-item index="1-1-1-2">3.0</el-menu-item>
-          </el-submenu>
-          <el-menu-item index="1-1-2">Vue-Router</el-menu-item>
-          <el-submenu index="1-1-3">
-            <template slot="title">数据管理</template>
-            <el-menu-item index="1-1-3-1">Vuex</el-menu-item>
-            <el-menu-item index="1-1-3-2">其他</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="1-2">
-          <template slot="title">Node</template>
-          <el-menu-item index="1-2-1">Koa</el-menu-item>
-          <el-menu-item index="1-2-2">Webpack</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="1-3">性能优化</el-menu-item>
-      </el-submenu>
-      <el-menu-item index="2">
-        <template slot="title">
-          <i class="el-icon-setting"></i>
-          <span>随笔</span>
-        </template>
-      </el-menu-item> -->
     </el-menu>
   </el-scrollbar>
 </template>
 
 <script>
-// import Xcollapse from './xcollapse'
 import Xelement from './xelement'
 export default {
   name: 'XcollapseTree',
