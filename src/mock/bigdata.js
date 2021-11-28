d308fd5e22af69eb316f5f2bd578a281f27e1830
xiao <183627391@qq.com>
2021-11-28 10:45:59 +0800

    add echarts demo

--- src/mock/bigdata.js
+++ src/mock/bigdata.js
@@ -1,21 +1,27 @@
-let nodes =  {};
-let links =  {};
-let categories =  {};
+let mockNodes = [];
+let mockLinks = [];
+let mockCates = [];
 
-nodes = Array.from({ length: 7000 }).map((item, index) => {
+mockNodes = Array.from({ length: 200 }).map((item, index) => {
   return ({
     id: String(index),
-    name: `name${index}`,
-    symbolSize: parseInt(Math.random() * 30),
+    name: `Name*${index}`,
+    symbolSize: parseInt(Math.random() * 32),
     x: parseFloat((Math.random() * 100).toFixed(2)),
     y: parseFloat((Math.random() * 50).toFixed(2)),
     value: parseFloat((Math.random() * 20).toFixed(2)),
     category: parseInt(Math.random() * 10),
   })
 })
+mockLinks = Array.from({ length: 200 }).map((item, index) => {
+  return ({
+    source: parseInt(20 - index - Math.random() * 10),
+    target: parseInt(index + Math.random() * 10),
+  })
+})
 
 export {
-  nodes,
-  links,
-  categories,
+  mockNodes,
+  mockLinks,
+  mockCates,
 }
--- src/views/Echarts/Demo.vue
+++ src/views/Echarts/Demo.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="echart-demo">
     <div class="demo-item">
-      <div :id="labelObj.relationId" class="item-echarts"></div>
+      <div id="canvas-relation" class="item-echarts"></div>
+    </div>
+    <div class="demo-item">
+      <div id="gl-relation" class="item-echarts"></div>
     </div>
   </div>
 </template>
 
 <script>
 import * as Echarts from 'echarts';
-import { nodes } from "@/mock/bigdata.js"
+import 'echarts-gl';
+import { mockNodes, mockLinks } from "@/mock/bigdata.js"
 const relationData = require("@/mock/relation.json");
-relationData.nodes = nodes;
+relationData.nodes = [ ...mockNodes ];
 
 export default {
   name: "EchartsDemo",
@@ -18,33 +22,39 @@ export default {
     return {
       labelObj: {
         relationText: "关系图",
-        relationId: "relation-demo",
       },
     }
   },
   mounted() {
     this.initEcharts();
+    this.initGlRelationEcharts();
     this.onEvent();
   },
   methods: {
     // 监听一些事件
     onEvent() {
-      if(this.relationEchart) {
-        window.onresize = () => this.relationEchart.resize();
+      if(this.relationEchart && this.glRelEchart) {
+        window.onresize = () => {
+          this.relationEchart.resize();
+          this.glRelEchart.resize();
+        };
         this.relationEchart.on("click", (params) => {
           console.log(params)
+        });
+        this.glRelEchart.on("click", (params) => {
+          console.log('gl', params)
         });
       }
     },
     initEcharts() {
       if(this.relationEchart) this.relationEchart.dispose();
-      this.relationEchart = Echarts.init(document.getElementById(this.labelObj.relationId));
+      this.relationEchart = Echarts.init(document.getElementById('canvas-relation'));
       // this.relationEchart.showLoading();
       // this.relationEchart.hideLoading();
       const graph = relationData;
       graph.nodes.forEach(function (node) {
         node.label = {
-          show: node.symbolSize > 90
+          show: node.symbolSize > 30
         };
       });
       let option = {
@@ -91,6 +101,57 @@ export default {
         ],
       };
       this.relationEchart.setOption(option);
+    },
+    // 初始化GL图表
+    initGlRelationEcharts() {
+      const colorList = [
+        'rgb(203,239,15)',
+        'rgb(73,15,239)',
+        'rgb(15,217,239)',
+        'rgb(30,15,239)',
+        'rgb(15,174,239)',
+        'rgb(116,239,15)',
+        'rgb(239,15,58)',
+        'rgb(15,239,174)',
+        'rgb(239,102,15)',
+        'rgb(239,15,15)',
+        'rgb(15,44,239)',
+        'rgb(239,145,15)',
+        'rgb(30,239,15)',
+        'rgb(239,188,15)',
+        'rgb(159,239,15)',
+        'rgb(159,15,239)',
+      ];
+      if(this.glRelEchart) this.glRelEchart.dispose();
+      this.glRelEchart = Echarts.init(document.getElementById('gl-relation'));
+      const nodes = mockNodes.map(item => (
+        { name: item.name, value: parseInt(Math.random()*100) }
+      ))
+      this.glRelEchart.setOption({
+        // backgroundColor: '#000',
+        // color: colorList,
+        series: [
+          {
+            type: 'graphGL',
+            nodes: nodes,
+            edges: mockLinks,
+            modularity: {
+              resolution: 2,
+              sort: true
+            },
+            lineStyle: {
+              color: 'rgba(255,0,0,1)',
+              with: 20,
+              opacity: 0.5
+            },
+            itemStyle: {
+              opacity: 1,
+              color: () => colorList[parseInt(Math.random()*10)],
+            },
+            symbolSize: () => parseInt(Math.random() * 100),
+          }
+        ]
+      });
     }
   }
 }
@@ -98,6 +159,7 @@ export default {
 
 <style lang="less" scoped>
 .echart-demo {
+  padding: 0 30px;
   .demo-item {
     padding: 10px;
     border: 1px dashed #eee;
